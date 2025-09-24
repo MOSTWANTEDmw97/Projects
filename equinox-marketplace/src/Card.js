42a377380916c96ece884bbb6ca3f1263732dc6c
Ritam <mostwanted.mw1r@gmail.com>
2025-09-24 17:46:58 +0530

    Minor changes

--- equinox-marketplace/src/Card.js
+++ equinox-marketplace/src/Card.js
@@ -2,13 +2,16 @@ import MainStyle from './Styles/MainStyle.module.scss';
 
 export default function Card({ title, description, image, link }) {
   return (
-    <div className={`card shadow-sm mb-4 ${MainStyle.card} card d-flex flex-column justify-content-between`}>
-      <img src={image} className="card-img-top" alt={title} />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-        <a href={link} className="btn btn-outline-primary">View</a>
+
+    <a href={link} style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>
+      <div className={`card shadow-sm mb-4 ${MainStyle.card} card d-flex flex-column justify-content-between`}>
+        <img src={image} className="card-img-top" alt={title}  sizes='10'/>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+        </div>
+    
       </div>
-    </div>
+    </a>
   );
 }
