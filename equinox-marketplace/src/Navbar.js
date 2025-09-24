229708262bebc9c590d7dc5a06371dde06613467
Ritam <mostwanted.mw1r@gmail.com>
2025-09-24 17:20:07 +0530

    Add Navbar component and integrate Bootstrap styles; update App.js and index.html

---
+++ equinox-marketplace/src/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <img src="/Images/EquinoxLogo.jpg" width="40px" />
+        <a className="navbar-brand" href="#">Equinox</a>
+      </div>
+    </nav>
+  );
+}
